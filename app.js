54462986cb5d575bf9eb2b5df7f802bff4ff0da1
A K Srivastava <anjanivaranasi123@gmail.com>
2021-10-10 17:54:37 +0530

    added procfile

--- app.js
+++ app.js
@@ -127,5 +127,13 @@ app.use(authRoutes);
 app.use(shopRoutes);
 app.use(adminRoutes);
 
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
 
-app.listen(3000);
+app.listen(port);
+
+app.listen(3000, function(){
+    console.log("server started on port 3000");
+});
